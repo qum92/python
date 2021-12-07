2ce051df991d91be717fe7fa8f92b62cb96a58d7
qum92 <oscar9992@naver.com>
2021-12-07 22:56:00 +0900

    누락된거 추가

--- package.py
+++ package.py
@@ -56,11 +56,13 @@
 # print(time.localtime())
 # print(time.strftime("%Y-%m-%d %H:%M:%S"))
 
-import datetime
+# import datetime
 # print("오늘 날짜는 ", datetime.date.today())
 
 # timedelta : 두 날짜 사이의 간격
-today = datetime.date.today() # 오늘 날짜 저장
-td = datetime.timedelta(days=100) # 100일 저장
-print("우리가 만난지 100일은", today + td) # 오늘부터 100일 후
+# today = datetime.date.today() # 오늘 날짜 저장
+# td = datetime.timedelta(days=100) # 100일 저장
+# print("우리가 만난지 100일은", today + td) # 오늘부터 100일 후
+
+
 
